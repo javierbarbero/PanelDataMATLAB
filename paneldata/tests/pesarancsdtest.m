9606fb363b40dadccb28fd910da19771b8c0393c
helix123 <kevintappe@gmx.de>
2017-09-17 16:06:11 +0200

    Update pesarancsdtest.m

--- paneldata/tests/pesarancsdtest.m
+++ paneldata/tests/pesarancsdtest.m
@@ -28,7 +28,7 @@ function [ test ] = pesarancsdtest( est )
     end
     
     if est.isInstrumental
-        error('Pesaran''s CSD test not valid for instrumetnal estimation')
+        error('Pesaran''s CSD test not valid for instrumental estimation')
     end
     
     % Create otuput structure
@@ -63,7 +63,7 @@ function [ test ] = pesarancsdtest( est )
                 
                 % Display error message if less than 2 common time periods
                 if length(Tij) < 2
-                    error('Somo units have 1 or less common time periods. Test cannot be performed.')
+                    error('Some units have 1 or less common time periods. Test cannot be performed.')
                 end
                 
                 % Get common time residuals for both groups
